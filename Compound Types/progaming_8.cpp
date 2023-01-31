75e6c214c5c043a225f7fcc3e4e1f0d961c334aa
Kimighwan <105644387+Kimighwan@users.noreply.github.com>
2023-01-31 14:43:35 +0900

    Create progaming_8.cpp
    4chapter compound types practice progaming
    
    progaming_8.cpp에서 구조체 변수 선언 대신 new를 사용하여 구조체에 대입.

---
+++ Compound Types/progaming_8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+struct Piza {
+	std::string name;//피자 회사의 이름(여러 단어로 될 수 있다)
+	double diameter;//피자의 지름
+	double wight;//피자의 중량
+};
+
+int main() {
+	Piza *piza = new Piza;
+
+	std::cout << "피자의 지름을 입력하시오 : ";
+	(std::cin>>piza->diameter).get();
+	std::cout << "피자 회사 이름을 입력하시오 : ";
+	getline(std::cin, piza->name);
+	std::cout << "피자의 중량을 입력하시오 : ";
+	(std::cin >> piza->wight).get();
+
+	std::cout << "피자 회사의 이름 : " << piza->name << std::endl;
+	std::cout << "피자의 지름 : " << piza->diameter << std::endl;
+	std::cout << "피자의 중량 : " << piza->wight <<"g" << std::endl;
+	
+	return 0;
+}
