b34c3ddce2a5e50f279db335e8d3271cdb7c10f7
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-09 16:14:54 +0900

    Update stock00.h

--- 10: Objects and Classes_example programing/stock/stock00.h
+++ 10: Objects and Classes_example programing/stock/stock00.h
@@ -11,7 +11,7 @@ private:
 public:
 	void acquire(const std::string& co, long n, double pr);
 	void buy(long num, double price);
-	void sell(long num, double pricee);
+	void sell(long num, double price);
 	void update(double price);
 	void show();
 };
