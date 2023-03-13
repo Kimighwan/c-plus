42f9431e16f2612f73905b9f7278d937fa63442b
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-13 16:56:06 +0900

    Create golf.h

---
+++ 10: Objects and Classes_practice programing/10.3/golf.h
@@ -0,0 +1,16 @@
+#ifndef GOLF_H
+#define GOLF_H
+class golf {
+private:
+	enum { Len = 40 };
+	char fullname[Len];
+	int handicap;
+public:
+	golf();
+	golf(const char* name, int hc); 
+	int setgolf(golf& g);
+	void sethandicap(int hc); 
+	void updatehandicap();
+	void showgolf() const; 
+};
+#endif
