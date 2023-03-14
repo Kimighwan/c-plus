d65b2c78817d243dc11a6261d0b7e1dc6ecb04f0
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-14 16:31:05 +0900

    Update golf.cpp

--- 10: Objects and Classes_practice programing/10.3/golf.cpp
+++ 10: Objects and Classes_practice programing/10.3/golf.cpp
@@ -10,30 +10,40 @@ golf::golf() {
 //비대화식 버전
 // 매개변수 값을 golf 구조체를 제공된 이름과 핸디캡으로 설정한다.
 golf::golf(const char* name, int hc) {
-	strncpy_s(g.fullname, name, Len);
-	g.handicap = hc;
+	strcpy_s(fullname, name);
+	sethandicap(hc);
 }
 // 이름과 핸디캡을 묻고, g의 멤버들을 입력된 값으로 설정한다. 이름이 입력되면 1리턴, 빈 이름이면 0을 리턴
-int golf(golf& g) {
-	std::cout << "이름을 입력하십시오: ";
-	std::cin.getline(g.fullname, Len);
+int golf::setgolf(golf& g) {
+	cout << "이름을 입력하십시오: ";
+	cin.getline(g.fullname, Len);
 	if (g.fullname[0] == '\0')
 		return 0;
-	std::cout << "핸디캡을 입력하십시오(정수형): ";
-	while (!(std::cin >> g.handicap))
-	{
-		std::cin.clear();
-		std::cout << "핸디캡을 입력하십시오(정수형): ";
-	}
-	while (std::cin.get() != '\n')
+	cout << "핸디캡을 입력하십시오(정수형): ";
+	cin >> g.handicap;
+	cin.clear();
+	while (cin.get() != '\n')
 		continue;
+	*this = golf(g.fullname, g.handicap);
+
 	return 1;
 }
 // handicap을 새로운 값으로 설정
-void handicap(golf& g, int hc) {
-	g.handicap = hc;
+void golf::sethandicap(int hc) {
+	handicap = hc;
 }
 // golf 구조체 내용 출력
-void showgolf(const golf& g) {
-	std::cout << "이름: " << g.fullname << ", 핸디캡: " << g.handicap << std::endl;
+void golf::showgolf() const {
+	cout << "이름: " << fullname << endl;
+	cout << "핸디캡: " << handicap << endl;
+}
+void golf::updatehandicap() {
+	int hc;
+	cout << this->fullname << "의 핸디캡 변경" << endl;
+	cout << "새 핸디캡: ";
+	cin >> hc;
+	handicap = hc;
+	cin.clear();
+	while (cin.get() != '\n')
+		continue;
 }
