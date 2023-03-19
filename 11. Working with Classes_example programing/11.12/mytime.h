a4ed94cd43e5b15c49afef21415304da152af609
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-19 13:46:17 +0900

    Create mytime.h

---
+++ 11. Working with Classes_example programing/11.12/mytime.h
@@ -0,0 +1,20 @@
+#ifndef MYTIME_H
+#define MYTIME_H
+#include <iostream>
+class Time {
+private:
+	int houres;
+	int minutes;
+public:
+	Time();
+	Time(int h, int m = 0);
+	void AddMin(int m);
+	void AddHr(int h);
+	void Reset(int h = 0, int m = 0);
+	Time operator+(const Time& t)const;
+	Time operator-(const Time& t)const;
+	Time operator*(double n)const;
+	friend Time operator*(double m, const Time& t) { return t * m; }
+	friend std::ostream& operator<<(std::ostream& os, const Time& t);
+};
+#endif 
