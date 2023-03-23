2180b8fed734b6477b408a9fe32802f7bfca73d9
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-23 15:50:11 +0900

    Create StringBad.h

---
+++ 12. Classes and Dynamic Memory Allocation_example programing/StringBad/StringBad.h
@@ -0,0 +1,15 @@
+#ifndef STRINGBAD_H	
+#define STRINGBAD_H
+#include <ostream>
+class StringBad {
+private:
+	char* str;
+	int len;
+	static int num_strings;
+public:
+	StringBad(const char* s);
+	StringBad();
+	~StringBad();
+	friend std::ostream& operator<<(std::ostream& os, const StringBad& st);
+};
+#endif
