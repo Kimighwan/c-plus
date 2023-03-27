7a28a7b71d8fad2af1b6c152d0825c121bbad724
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-03-27 15:30:22 +0900

    Create stack1.cpp

---
+++ 12. Classes and Dynamic Memory Allocation_example programing_example programing/12.4/stack1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "stack1.h"
+Stack::Stack(int n) {
+	size = n;
+	pitems = new Item[n];
+	top = 0;
+
+}
+
+Stack::Stack(const Stack& st) {
+	pitems = new Item[st.size];
+	size = st.size;
+	top = st.top;
+
+	for (int i = 0; i < top; i++)
+		pitems[i] = st.pitems[i];
+}
+
+Stack::~Stack() {
+	delete[] pitems;
+}
+
+bool Stack::isempty() const {
+	return top == 0;
+}
+bool Stack::isfull() const {
+	return top == size;
+}
+bool Stack::push(const Item& item) {
+	if (top < size)
+	{
+		pitems[top++] = item;
+		return true;
+	}
+	else
+		return false;
+}
+bool Stack::pop(Item& item) {
+	if (top > 0)
+	{
+		item = pitems[--top];
+		return true;
+	}
+	return false;
+}
+Stack& Stack::operator=(const Stack& st) {
+	if (this == &st)
+		return *this;
+	delete[] pitems;
+	pitems = new Item[st.size];
+	top = st.top;
+	size = st.size;
+
+	for (int i = 0; i < top; i++)
+		pitems[i] = st.pitems[i];
+
+	return *this;
+}
