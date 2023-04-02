71ae0aa1de8bdd05d7cdeaabb57268b8dbde3872
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-04-02 23:11:07 +0900

    Update port.h

--- 13. Class Inheritance/practice programing/4/port.h
+++ 13. Class Inheritance/practice programing/4/port.h
@@ -1,19 +1,20 @@
 #ifndef PORT_H
 #define PROT_H
+#pragma warning(disable:4996)
 #include <iostream>
 using namespace std;
 class Port {
 private:
 	char* brand;
-	char* style[20]; //tawny, ruby, vintage
+	char style[20]; //tawny, ruby, vintage
 	int bottles;
 public:
 	Port(const char* br = "none", const char* st = "none", int b = 0);
 	Port(const Port& p);
 	virtual ~Port() { delete[] brand; }
-	Port& operator=(const Port& p);
-	Port& operator+=(int b);
-	Port& operator-=(int b);
+	Port& operator=(const Port& p); //복사 생성자
+	Port& operator+=(int b); // bottles에 b를 더한다
+	Port& operator-=(int b); //가능할 경우 bottles에서 b를 뺀다
 	int BottleCount() const { return bottles; }
 	virtual void Show() const;
 	friend ostream& operator<<(ostream& os, const Port& p);
@@ -24,10 +25,9 @@ private:
 	char* nickname;
 	int year;
 public:
-	VintagePort();
-	VintagePort(const char* br, int b, const char* nn, int y);
+	VintagePort(const char* br = "none", const char* st = "none", int b = 0, const char* nn = "none", int y = 0);
 	VintagePort(const VintagePort& vp);
-	virtual ~VintagePort() { delete[] nickname; }
+	~VintagePort() { delete[] nickname; }
 	VintagePort& operator=(const VintagePort& vp);
 	void Show() const;
 	friend ostream& operator<<(ostream& os, const VintagePort& vp);
