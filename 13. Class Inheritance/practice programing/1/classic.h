b993a426a17109ff5b43490d74261abc7abd7b6a
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-04-02 14:41:53 +0900

    Update classic.h

--- 13. Class Inheritance/practice programing/1/classic.h
+++ 13. Class Inheritance/practice programing/1/classic.h
@@ -1,5 +1,6 @@
 #ifndef CLASSIC_H
 #define CLASSIC_H
+#pragma warning(disable:4996)
 class Cd {                   //cd 디스크
 private:
 	char performers[50];
@@ -7,9 +8,8 @@ private:
 	int selections; //수록 곡 수
 	double playtime; // 분 단위의 연주 시간
 public:
-	Cd(const char* s1 ="none",const char* s2="none", int n=0, double x=0.0);
-	Cd(const Cd& d);
-	virtual ~Cd(); 
+	Cd(const char* s1 = "none", const char* s2 = "none", int n = 0, double x = 0.0);
+	virtual ~Cd() {}
 	virtual void Report() const; // Cd의 모든 데이터를 보고
 	Cd& operator=(const Cd& d);
 };
@@ -20,8 +20,8 @@ private:
 public:
 	Classic(const char* t = "none", const char* s1 = "none", const char* s2 = "none", int n = 0, double x = 0.0);
 	Classic(char* t, Cd& d);
-	virtual ~Classic();
+	virtual ~Classic() {}
 	virtual void Report() const;
-	Cd& operator=(const Classic& cc);
+	Classic& operator=(const Classic& cc);
 };
 #endif
