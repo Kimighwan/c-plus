eb19525c0b28619453c2d9b7d4c0eb20fbe146de
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-05-04 16:16:52 +0900

    Create strfile.cpp

---
+++ 16: The string Class and the Standard Template Library/example programing/strfile/strfile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+int main() {
+	using namespace std;
+	ifstream fin;
+	fin.open("C:\\ABC.txt");
+	if (fin.is_open() == false)
+	{
+		cerr << "파이을 열 수 없습니다.\n";
+		exit(EXIT_FAILURE);
+	}
+	string item;
+	int count = 0;
+	getline(fin, item, ':');
+	while (fin)
+	{
+		++count;
+		cout << count << ": " << item << endl;
+		getline(fin, item, ':');
+	}
+	cout << "프로그램을 종료합니다.\n";
+	fin.close();
+	return 0;
+}
