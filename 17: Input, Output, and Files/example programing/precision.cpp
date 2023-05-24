352387f0424c78cff9c619cce00432e48478abe8
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-05-24 15:41:45 +0900

    Create precision.cpp

---
+++ 17: Input, Output, and Files/example programing/precision.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main() {
+	using namespace std;
+	float price1 = 20.40;
+	float price2 = 1.9 + 8.0 / 9.0;
+
+	cout << "\"손오공의 인형\" $" << price1 << "!\n";
+	cout << "\"사오정의 인형\" $" << price2 << "!\n";
+
+	cout.precision(2);
+	cout << "\"손오공의 인형\" $" << price1 << "!\n";
+	cout << "\"사오정의 인형\" $" << price2 << "!\n";
+
+	return 0;
+}
