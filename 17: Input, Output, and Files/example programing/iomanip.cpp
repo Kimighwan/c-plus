08ff99d659a10bfcc584a9883a1532c9a718c831
Kimikhwan <105644387+Kimighwan@users.noreply.github.com>
2023-06-05 22:41:22 +0900

    Create iomanip.cpp

---
+++ 17: Input, Output, and Files/example programing/iomanip.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+int main() {
+	using namespace std;
+	cout << fixed << right;
+	cout << setw(6) << "N" << setw(14) << "제곱근" << setw(15) << "네제곱근\n";
+
+	double root;
+	for (int n = 10; n <= 100; n += 10)
+	{
+		root = sqrt(double(n));
+		cout << setw(6) << setfill('.') << n << setfill(' ') << setw(12) << setprecision(3) << root
+			<< setw(14) << setprecision(4) << sqrt(root) << endl;
+	}
+	return 0;
+}
